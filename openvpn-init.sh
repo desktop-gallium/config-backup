b2b952228791f5e4e6d3820065e3cbc920e12845
desktop-gallium <54791933+desktop-gallium@users.noreply.github.com>
2021-11-09 17:26:21 +0900

    Add files via upload

---
+++ openvpn-init.sh
@@ -0,0 +1,4 @@
+ls -lah "/Applications/OpenVPN Connect/"
+ls -lah "/Applications/OpenVPN Connect/OpenVPN Connect.app/"
+ls -lah "/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/"
+ls -lah "/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/"
