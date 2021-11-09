b0e2285f3cb9feb1c9c6b45559395c2c5c2e548d
desktop-gallium <54791933+desktop-gallium@users.noreply.github.com>
2021-11-09 17:22:16 +0900

    Update openvpn-init.sh

--- openvpn-init.sh
+++ openvpn-init.sh
@@ -1,3 +1,4 @@
-"/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/OpenVPN Connect" --set-setting=confirmation-dialogs --value=none
-"/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/OpenVPN Connect" --set-setting=timeout --value=0
-"/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/OpenVPN Connect" --set-setting=launch-options --value=connect-latest
+ls -lah "/Applications/OpenVPN Connect/"
+ls -lah "/Applications/OpenVPN Connect/OpenVPN Connect.app/"
+ls -lah "/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/"
+ls -lah "/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/"
