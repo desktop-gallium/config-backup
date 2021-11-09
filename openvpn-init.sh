9529b97881e37cfdcacc44fe8d99d3f9a8bc73fd
desktop-gallium <54791933+desktop-gallium@users.noreply.github.com>
2021-11-09 17:03:27 +0900

    Add files via upload

---
+++ openvpn-init.sh
@@ -0,0 +1,3 @@
+/Applications/OpenVPN\ Connect/OpenVPN\ Connect.app/Contents/MacOS/OpenVPN\ Connect --set-setting=confirmation-dialogs --value=none
+/Applications/OpenVPN\ Connect/OpenVPN\ Connect.app/Contents/MacOS/OpenVPN\ Connect --set-setting=timeout --value=0
+/Applications/OpenVPN\ Connect/OpenVPN\ Connect.app/Contents/MacOS/OpenVPN\ Connect --set-setting=launch-options --value=connect-latest
